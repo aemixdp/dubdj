1959b28bc0365b47186745db3411f74f435f9b94
aemxdp <andrii.s.polishchuk@gmail.com>
2016-01-15 08:36:46 +0200

    Add example config

---
+++ config.example.js
@@ -0,0 +1,16 @@
+module.exports = {
+    room: 'rrrrrrrroom',
+    googleApiKey: 'AFDAsdfadfadsFAFsaDFafdsf-DSFadfafd',
+    apiPassword: 'asjdisajdoisajdi32j9d',
+    profiles: [
+        {
+            credentials: {
+                username: 'qweqeqweqwe',
+                password: '32132131234'
+            },
+            playlists: [
+                'SFDSFewf32F@#feWFEWFwfwefw-fEWfewfwe'
+            ]
+        }
+    ]
+};
